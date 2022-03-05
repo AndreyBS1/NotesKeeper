c0262b97db8d0e4417cb801898f1562664d24a3a
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-03-05 18:50:06 +0300

    Pre-alpha build v.2

--- src/views/MainScreen.vue
+++ src/views/MainScreen.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="main-screen">
+  <div v-show="visible" class="main-screen">
+    <div class="instruments">
+      <b-button
+        pill
+        variant="outline-dark"
+        @click="clickOnCreateButton()"
+        >
+          New Note
+        </b-button>
+    </div>
     <note-card class="note-card"/>
     <note-card class="note-card"/>
     <note-card class="note-card"/>
@@ -16,21 +25,38 @@ export default {
   name: 'MainScreen',
   components: {
     NoteCard,
+  },
+  data() {
+    return {
+      visible: true,
+    }
+  },
+  methods: {
+    clickOnCreateButton() {
+      this.visible = false;
+    }
   }
 }
 </script>
 
 <style scoped>
 .main-screen {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    flex-wrap: wrap;
-    padding: 3% 5%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 3% 5%;
+}
+
+.instruments {
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .note-card {
-    width: 30%;
-    margin: 3% 0;
+  width: 30%;
+  margin: 3% 0;
 }
 </style>
