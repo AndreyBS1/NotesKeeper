be370a09fbbe3467cbafb964a57565a8ecc9b0aa
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-03-08 22:18:18 +0300

    Changed NoteScreen design

--- src/views/NoteScreen.vue
+++ src/views/NoteScreen.vue
@@ -1,13 +1,46 @@
 <template>
-    <div>
-        <div>
-            {{ note.note_title }}
-            <b-button @click="clickOnBackButton">Back</b-button>
-        </div>
-        <div>
-            {{ note.note_text }}
-        </div>
-    </div>    
+    <div class="note-screen">
+        <b-form>
+            <div class="title-input-area">
+                <b-form-group class="title-input">
+                    <b-form-input
+                        size="lg"
+                        v-model="noteObject.note_title"
+                        placeholder="Note title"
+                        required
+                    ></b-form-input>
+                </b-form-group>
+                <div class="button">
+                    <b-button
+                        pill
+                        variant="outline-dark"
+                    >
+                        Save
+                    </b-button>
+                </div>
+                <div class="button">
+                    <b-button
+                        pill
+                        variant="outline-dark"
+                        @click="clickOnBackButton"
+                    >
+                        Back
+                    </b-button>
+                </div>
+            </div>
+            <b-form-group class="text-input-area">
+                <b-form-textarea
+                    class="text-input"
+                    rows="13"
+                    max-rows="100"
+                    no-resize
+                    v-model="noteObject.note_text"
+                    placeholder="Note text"
+                    required
+                ></b-form-textarea>
+            </b-form-group>
+        </b-form>
+    </div>
 </template>
 
 <script>
@@ -25,6 +58,12 @@ export default {
 
     emits: ['back-to-main-screen'],
 
+    computed: {
+        noteObject() {
+            return this.note;
+        }
+    },
+
     methods: {
         clickOnBackButton() {
             this.$emit('back-to-main-screen');
@@ -34,5 +73,29 @@ export default {
 </script>
 
 <style scoped>
+.note-screen {
+    padding: 3% 5%;
+}
 
+.title-input-area {
+    display: flex;
+    /* justify-content: space-between; */
+}
+
+.title-input {
+    margin-right: 1%;
+    width: 100%;
+}
+
+.button {
+    margin: 0 1%;
+}
+
+.text-input-area {
+    margin: 3% 0;
+}
+
+.text-input {
+    width: 100%;
+}
 </style>
